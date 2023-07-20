37416bc28ce916a3838b9b075fbf67b924ff3bd3
Chris Rueber <crueber@gmail.com>
2023-07-20 15:04:41 -0500

    Cleaning sh and bin

--- install.sh
+++ install.sh
@@ -1,41 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
-cutstring="DO NOT EDIT BELOW THIS LINE"
+skip=('install.sh' 'README.md' 'LICENSE' 'nvim-custom')
 
 for name in *; do
+  if [[ " ${skip[*]} " =~ " ${name} " ]]; then
+    continue
+  fi
+
   target="$HOME/.$name"
-  if [ -e "$target" ]; then
-    if [ ! -L "$target" ]; then
-      cutline=`grep -n -m1 "$cutstring" "$target" | sed "s/:.*//"`
-      if [ -n "$cutline" ]; then
-        cutline=$((cutline-1))
-        echo "Updating $target"
-        head -n $cutline "$target" > update_tmp
-        startline=`sed '1!G;h;$!d' "$name" | grep -n -m1 "$cutstring" | sed "s/:.*//"`
-        if [ -n "$startline" ]; then
-          tail -n $startline "$name" >> update_tmp
-        else
-          cat "$name" >> update_tmp
-        fi
-        mv update_tmp "$target"
-      else
-        echo "WARNING: $target exists but is not a symlink."
-      fi
-    fi
+  if [ -L "$target" ]; then
+    echo "INFO: $target symlink exists."
+  elif [ -e "$target" ] && [ ! -L "$target" ]; then
+    echo "WARNING: $target exists but is not symlinked."
   else
-    if [ "$name" != 'install.sh' ] && [ "$name" != 'README.md' ] && [ "$name" != 'LICENSE' ] && [ "$name" != "nvim-custom" ]; then
-      echo "Creating $target"
-      if [ -n "$(grep "$cutstring" "$name")" ]; then
-        cp "$PWD/$name" "$target"
-      else
-        ln -s "$PWD/$name" "$target"
-      fi
-    fi
+    echo "Symlinking '$PWD/$name' to '$target'"
+    ln -s "$PWD/$name" "$target"
   fi
 done
 
 if [ -e "$HOME/.oh-my-zsh/themes" ]; then
-  cp $PWD/zsh-themes/* "$HOME/.oh-my-zsh/themes/"
+  cp -r $PWD/zsh-themes/* "$HOME/.oh-my-zsh/themes/"
 fi
 
 NVIM="$HOME/.config/nvim"
