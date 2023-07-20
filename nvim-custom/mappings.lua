7270132b9d21ba4ed106801e35cd5094d3cb2675
Chris Rueber <crueber@gmail.com>
2023-07-20 18:47:41 -0500

    nvim mapping update

--- nvim-custom/mappings.lua
+++ nvim-custom/mappings.lua
@@ -9,7 +9,8 @@ M.base46 = {
       end,
       "toggle transparency",
     },
-    ["<leader>pf"] = {"<cmd>Telescope projects<CR>", "Find files in project"}
+    ["<leader><leader>"] = {"<c-^>", "toggle buffers"},
+    ["<leader>tp"] = {"<cmd>Telescope projects<CR>", "Find files in project"}
   },
 }
 
