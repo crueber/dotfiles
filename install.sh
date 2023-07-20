a47c1ac223488e9f2da56a7182c2bc55fd3ca5fa
Chris Rueber <crueber@gmail.com>
2023-07-20 15:41:47 -0500

    Making the install script better.

--- install.sh
+++ install.sh
@@ -1,42 +1,61 @@
 #!/bin/bash
 
-skip=('install.sh' 'README.md' 'LICENSE' 'nvim-custom')
+link () {
+  name=$1
+  target=$2
 
-for name in *; do
-  if [[ " ${skip[*]} " =~ " ${name} " ]]; then
-    continue
-  fi
-
-  target="$HOME/.$name"
   if [ -L "$target" ]; then
     echo "INFO: $target symlink exists."
   elif [ -e "$target" ] && [ ! -L "$target" ]; then
     echo "WARNING: $target exists but is not symlinked."
   else
-    echo "Symlinking '$PWD/$name' to '$target'"
-    ln -s "$PWD/$name" "$target"
+    echo "Symlinking '$name' to '$target'"
+    ln -s "$name" "$target"
   fi
+}
+
+skip=('config' 'install.sh' 'README.md' 'LICENSE' 'nvim-custom' 'zsh-themes')
+for name in *; do
+  if [[ " ${skip[*]} " =~ " ${name} " ]]; then
+    continue
+  fi
+
+  link $name "$HOME/.$name"
+done
+
+if [ ! -e "$HOME/.config" ]; then
+  echo "INFO: $HOME/.config does not exist, creating."
+  mkdir $HOME/.config
+else
+  echo "INFO: Ensured $HOME/.config exists."
+fi
+
+for name in config/*; do
+  link $name "$HOME/.$name"
 done
 
 if [ -e "$HOME/.oh-my-zsh/themes" ]; then
+  echo "INFO: Copying oh-my-zsh themes to the $HOME/.oh-my-zsh/themes/."
   cp -r $PWD/zsh-themes/* "$HOME/.oh-my-zsh/themes/"
+else
+  echo "WARNING: Oh-my-zhs theme directory not found, skipping theme copy."
 fi
 
 NVIM="$HOME/.config/nvim"
 if [ -e "$NVIM" ] && [ ! -e "$NVIM/.git" ]; then
-  echo "Removing $NVIM because it is not a git repo."
+  echo "INFO: Removing $NVIM because it is not a git repo."
   rm -rf $NVIM
 fi
 if [ ! -e "$NVIM" ]; then
-  echo "Cloning in to nvChad at $NVIM."
+  echo "INFO: Cloning in to nvChad at $NVIM."
   git clone https://github.com/nvchad/nvchad "$NVIM" --depth 1
 fi
 if [ -e "$NVIM/lua" ]; then 
   if [ -e "$NVIM/lua/custom" ]; then
-    echo "Removing $NVIM/lua/custom."
+    echo "INFO: Removing $NVIM/lua/custom."
     rm -rf "$NVIM/lua/custom"
   fi
-  echo "Copying custom lua scripts for nvim in to $NVIM/lua/custom"
+  echo "INFO: Copying custom lua scripts for nvim in to $NVIM/lua/custom"
   cp -r nvim-custom "$NVIM/lua/custom"
 fi 
 
